a2922bdca6b1cb59d95107271bcac940734b2ffa
Dmitry <dmitry1939@gmail.com>
2016-05-07 11:13:39 +0300

    header checkpoint

--- src/js/develop/develop_1.js
+++ src/js/develop/develop_1.js
@@ -1,4 +1,18 @@
+/* header slider init */
 
+    function headerSliderInit(){
+
+        $('.flags-slider-wrap').slick({
+            lazyLoad: 'ondemand',
+            infinite:true,
+            slidesToShow:3,
+            slidesToScroll:1,
+            dots:false
+        });
+
+    }
+
+/* /header slider init */
 
 
 $(document).ready(function(){
@@ -6,6 +20,8 @@ $(document).ready(function(){
 });
 
 $(window).load(function(){
+
+    headerSliderInit();
 
 });
 
