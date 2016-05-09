bf16b38d70ae9096dea252de5921983477ae2765
Dmitry <dmitry1939@gmail.com>
2016-05-09 08:43:21 +0300

    header done

--- src/js/develop/develop_1.js
+++ src/js/develop/develop_1.js
@@ -14,22 +14,37 @@
 
 /* /header slider init */
 
-/* header menu nav toogle */
+/* header menu nav toogle & header-list index */
 
     function toogleHeaderNav(){
 
-        $('.header-bottom li').hover(
-            function(){
-                $(this).find('ul').stop().slideDown(300);
-            },
-            function(){
-                $(this).find('ul').stop().slideUp(300);
-            }
-        );
+        /* nav toggle */
+
+            $('.header-bottom li').hover(
+                function(){
+                    $(this).find('ul').stop().slideDown(300);
+                },
+                function(){
+                    $(this).find('ul').stop().slideUp(300);
+                }
+            );
+
+        /* /nav toggle */
+
+        /* z-index */
+
+            var navLength = $('.header-bottom nav>ul>li').length;
+
+            $('.header-bottom nav>ul>li').each(function(){
+                $(this).css({'z-index':navLength});
+                navLength--;
+            });
+
+        /* /z-index */
 
     };
 
-/* header menu nav toggle */
+/* header menu nav toggle & header-list index */
 
 
 $(document).ready(function(){
