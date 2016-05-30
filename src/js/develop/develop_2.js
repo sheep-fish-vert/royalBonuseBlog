df091a307fd78b52f786bc648f3c6968abb6c72f
Art G <artem.garmatyk@gmail.com>
2016-05-30 09:24:11 +0300

    add

--- src/js/develop/develop_2.js
+++ src/js/develop/develop_2.js
@@ -1,34 +1,8 @@
-
-
-
 $(document).ready(function(){
 
     $('select[name=search_select]').on('change', function(){
         
-        $(this).closest('form').submit();
-    /*
-        var thisForm = $(this).closest('form');
-        var formSur = thisForm.serialize();
-        
-
-        $.ajax({
-            url : thisForm.attr('action'),
-            data: formSur,
-            method:'POST',
-            success : function(data){
-               
-                if ( data.trim() == 'true') {
-                    thisForm.trigger("reset");
-                    popNext("#call_success", "call-popup");
-                }
-                else {
-                thisForm.trigger('reset');
-                }
-               
-
-            }
-        });
-    */
+        window.location = $(this).val();
         
     });
     
